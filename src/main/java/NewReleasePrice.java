d1c6153a343abf7afb6e72b2efe7f09f2576c513
Morel Chang (RD-TW) <morel_chang@trendmicro.com>
2022-01-24 14:54:51 +0800

    Replace conditional in Price.getCharge() with polymorphism

--- src/main/java/NewReleasePrice.java
+++ src/main/java/NewReleasePrice.java
@@ -6,4 +6,8 @@ public class NewReleasePrice extends Price {
         return Movie.NEW_RELEASE;
     }
 
+    @Override
+    double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
 }
